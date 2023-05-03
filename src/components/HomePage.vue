0c0e8ecb7eb12e750c338895bc5bf4c5f177da97
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-05-03 15:30:04 -0300

    Criando card para cada produto da lista, importando dados do Faker, e renderizando na tela a lista com sucesso

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="HomePage">
 
-    <h1>Hello World com Vue!</h1>
+    <h1>Camisetas</h1>
+    <div class="home-page">
+    <div v-for="product in products" :key="product.id" class="product-item">
+      <img :src="product.image" />
+      <h2>{{ product.name }}</h2>
+      <p>{{ product.description }}</p>
+      <p>{{ product.price }}</p>
+    </div>
+  </div>
   </div>
  </template>
  
  <script>
+ import { faker } from '@faker-js/faker';
  export default {
    name: 'HomePage',
    props: {
      msg: String
-   }
+   },
+   data() {
+    return {
+      products: []
+    };
+  },
+  mounted() {
+    this.generateProducts();
+  },
+  methods: {
+    generateProducts() {
+      for (let i = 0; i < 15; i++) {
+        const product = {
+          id: i,
+          name: faker.commerce.productName(),
+          image: faker.image.people(),
+          description: faker.commerce.productDescription(),
+          price: faker.commerce.price()
+        };
+        this.products.push(product);
+      }
+    }
+  }
  }
  </script>
  
