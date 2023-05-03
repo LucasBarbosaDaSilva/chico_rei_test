44801e5b027be7322a2e787865acc8b2eed35840
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-05-03 14:51:19 -0300

    Criando componente HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello World com Vue!</h1>
+ </template>
+ 
+ <script>
+ export default {
+   name: 'HomePage',
+   props: {
+     msg: String
+   }
+ }
+ </script>
+ 
+ 
+ <style scoped>
+ 
+ </style>
